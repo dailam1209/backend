48a742f58e95c96dd7492ff3a19a2f9b0b091762
dailam1209 <lamngocdai050920@gmail.com>
2024-06-13 10:27:28 +0700

    update stripe

--- routers/StripeRoutes.js
+++ routers/StripeRoutes.js
@@ -4,13 +4,15 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const router = express.Router();
 
-router.post('/create-checkout-session', async (req, res) => {
+app.post('/create-checkout-session', async (req, res) => {
   try {
     const { amount, name, email } = req.body;
     
+    // Chuyển đổi amount từ USD sang cent
+    const amountInCents = Math.round(amount * 100);
    
-    const api = await stripe.paymentIntents.create({
-      amount: amount,
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amountInCents,
       currency: 'usd',
       automatic_payment_methods: {
         enabled: true,
@@ -20,9 +22,10 @@ router.post('/create-checkout-session', async (req, res) => {
         customer_email: email
       }
     });
+    
     res.send({
-      idInitSecret: api
-    })
+      idInitSecret: paymentIntent.client_secret
+    });
    
   } catch (e) {
     res.status(500).send({ error: e.message });
